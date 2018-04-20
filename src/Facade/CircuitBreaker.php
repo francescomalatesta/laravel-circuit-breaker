c214cb624a1e5ec3effae87c24d25f8abf1f56a7
Francesco Malatesta <hellofrancesco@gmail.com>
2018-04-20 21:06:31 +0200

    Add facade with autodiscovery

---
+++ src/Facade/CircuitBreaker.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FrancescoMalatesta\LaravelCircuitBreaker\Facade;
+
+
+use FrancescoMalatesta\LaravelCircuitBreaker\Manager\CircuitBreakerManager;
+use Illuminate\Support\Facades\Facade;
+
+class CircuitBreaker extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return CircuitBreakerManager::class;
+    }
+}
